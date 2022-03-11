797aa966f2fea64a3e207b04a3016054102c5c6c
John Diercks <jdpivotjohn@gmail.com>
2022-03-10 20:32:43 -0600

    The first upload of THE GREATEST SITE EEVVEERRRgit add -Agit add -A

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>JohnickTech Portfolio Website</title>
+</head>
+<body>
+    <nav>
+        <!-- where the navigation is going to go -->
+    </nav>
+    <section>
+        <!-- about -->
+    </section>
+    <section>
+        <!-- contact form -->
+        <form action="">
+
+        </form>
+    </section>
+    <footer>
+
+    </footer>
+</body>
+</html>
