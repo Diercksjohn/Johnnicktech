498274b4db573c0379bd3d9692343fd4f6af0a6a
John Diercks <jdpivotjohn@gmail.com>
2022-03-15 19:27:23 -0500

    placeholder text and basic form add

--- index.php
+++ index.php
@@ -4,20 +4,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles/styles.css">
     <title>JohnickTech Portfolio Website</title>
 </head>
 <body>
     <nav>
         <!-- where the navigation is going to go -->
+        <ul>
+            <li>JohnickTech</li>
+        </ul>
+        <ul>
+            <li>About</li>
+            <li>Web Development and Design</li>
+            <li>Computer Repair</li>
+        </ul>
+        <ul>
+            <li>Contact</li>
+        </ul>
     </nav>
     <section>
         <!-- about -->
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit cumque iure ab officia vitae doloremque odio nobis ratione! Saepe officiis nobis, autem omnis quaerat excepturi eaque deleniti sint delectus repellat.
+        Praesentium neque ut repellat molestias est adipisci obcaecati officiis. Consequatur, quo explicabo, sapiente recusandae facilis delectus laudantium aliquam consectetur ullam cum excepturi, tempora necessitatibus sed exercitationem minima expedita distinctio at.
+        Dignissimos alias dolores id, nostrum suscipit vero eveniet totam nesciunt hic laborum qui sint delectus ullam beatae nihil tenetur numquam ducimus eligendi quisquam ratione est sequi pariatur consequuntur doloribus. Sit.
+        Aperiam harum sunt suscipit consequuntur dolore, tempora quas culpa neque, voluptatibus praesentium perferendis nisi velit expedita molestiae voluptate laboriosam! Doloribus cumque in veritatis incidunt delectus ullam. Magni natus praesentium itaque!
+        Quo dolor, eius sunt tenetur quisquam architecto magnam laborum voluptatibus eos ullam qui non odit aliquam consequuntur vel mollitia? Quisquam quam nulla fugiat alias labore nobis aperiam ex laudantium consequatur.
     </section>
-    <section>
+    <section id="form">
         <!-- contact form -->
-        <form action="">
-
+        <form action="index.php" method="POST" id="form_main">
+            <label for="firstName">First Name</label>
+            <input type="text" name="firstName" id="firstName">
+            <label for="firstName">Last Name</label>
+            <input type="text" name="lastName" id="lastName">
+            <label for="inquery">Describe the Issue</label>
+            <textarea name="inquery" id="inquery" cols="30" rows="10"></textarea>
+            <button type="submit" value="Submit">Send Message</button>
         </form>
     </section>
     <footer>
